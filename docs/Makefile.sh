4ea1c98f44ec7ce8c8e416d6554134be8c7e2967
Alkis Georgopoulos <alkisg@gmail.com>
2019-08-18 07:39:49 +0300

    Rename man to docs

--- docs/Makefile.sh
+++ docs/Makefile.sh
@@ -10,9 +10,27 @@
 # To get a list of all lts.conf parameters, run:
 # echo $(grep -r LTSP.CONF | sed 's/.*LTSP.CONF://' | tr ' ' '\n' | sort -u)
 
-VERSION=$(. ../ltsp/common/ltsp/55-ltsp.sh && echo "$_VERSION")
+footer() {
+    cat <<EOF
+## COPYRIGHT
+Copyright 2019 the LTSP team, see AUTHORS
+
+## SEE ALSO
+**ltsp**(8), **ltsp.conf**(5), **ltsp-dnsmasq**(8), **ltsp-image**(8),
+**ltsp-info**(8), **ltsp-initrd**(8), **ltsp-ipxe**(8), **ltsp-kernel**(8),
+**ltsp-nfs**(8)
+EOF
+}
+
+set -e
+cd "${1%/*}"
+if command -v dpkg-parsechangelog >/dev/null; then
+    VERSION=$(dpkg-parsechangelog -l ../debian/changelog -S VERSION)
+else
+    VERSION=$(. ../ltsp/common/ltsp/55-ltsp.sh && echo "$_VERSION")
+fi
 date=$(date "+%Y-%m-%d")
-rm -rf ../man/man
+rm -rf man
 for mp in *.[0-9].md; do
     applet_section=${mp%.md}
     applet=${applet_section%.[0-9]}
@@ -26,13 +44,7 @@ for mp in *.[0-9].md; do
 $applet($section) -- $description
 =====================================
 $(sed "1,2d" "$mp")
-## COPYRIGHT
-Copyright 2019 the LTSP team, see AUTHORS
-
-## SEE ALSO
-**ltsp**(8), **ltsp.conf**(5), **ltsp-dnsmasq**(8), **ltsp-image**(8),
-**ltsp-info**(8), **ltsp-initrd**(8), **ltsp-ipxe**(8), **ltsp-kernel**(8),
-**ltsp-nfs**(8)
+$(footer)
 EOF
         test -d ../../ltsp.github.io/docs/ || continue
         mkdir -p "../../ltsp.github.io/docs/$applet"
@@ -41,26 +53,14 @@ EOF
 $applet($section) -- $description
 =====================================
 $(sed "1,2d" "$mp")
-## COPYRIGHT
-Copyright 2019 the LTSP team, see AUTHORS
-
-## SEE ALSO
-**ltsp**(8), **ltsp.conf**(5), **ltsp-dnsmasq**(8), **ltsp-image**(8),
-**ltsp-info**(8), **ltsp-initrd**(8), **ltsp-ipxe**(8), **ltsp-kernel**(8),
-**ltsp-nfs**(8)
+$(footer)
 EOF
     else
         go-md2man > "man/man$section/$applet.$section" <<EOF
 $applet $section $date "LTSP $VERSION"
 =====================================
 $(cat "$mp")
-## COPYRIGHT
-Copyright 2019 the LTSP team, see AUTHORS
-
-## SEE ALSO
-**ltsp**(8), **ltsp.conf**(5), **ltsp-dnsmasq**(8), **ltsp-image**(8),
-**ltsp-info**(8), **ltsp-initrd**(8), **ltsp-ipxe**(8), **ltsp-kernel**(8),
-**ltsp-nfs**(8)
+$(footer)
 EOF
         # TODO: work around https://github.com/cpuguy83/go-md2man/issues/26
         sed 's/\\~/~/g' -i "man/man$section/$applet.$section"
